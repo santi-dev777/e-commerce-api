00ed1955518846c0ff629940ebdcfdf61391307d
santi-dev777 <santi.dev777@gmail.com>
2025-04-29 14:24:31 -0300

    model of categories, crud and connection to db

--- src/models/categorires-model.js
+++ src/models/categorires-model.js
@@ -8,4 +8,49 @@ export class CategoryModel {
         );
         return categories;
     }
+
+    static async getById( id ){
+        const [category] = await pool.query(
+            'SELECT * FROM categories WHERE id = ?', [id]
+        )
+        return  category[0]
+    }
+
+    static async create({ input }){
+        const {name, description} = input
+
+        const [result] = await pool.query(
+             `INSERT INTO categories (name, description)
+             VALUES (?, ?)`,
+             [name, description]
+        )
+
+        if (result.affectedRows === 0) return false
+
+        return this.getById(result.insertId)
+    }
+
+    static async delete ({ id }){
+        const [rows] = await pool.query(
+            "DELETE FROM category WHERE id = ?", [id]
+        )
+
+        if(rows.affectedRows <= 0) return false
+    }
+
+    static async update ({ id, input }){
+        const { name, description } = input
+
+        const [ rows ] = await pool.query(
+            `UPDATE category 
+            SET name = IFNULL(?, name),
+            description = IFNULL(?, description)
+            WHERE id = ?`,
+            [name, description, id]
+        )
+
+        if(rows.affectedRows === 0) return false
+
+        return this.getById(id)
+    }
 }
